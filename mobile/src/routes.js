a1b17b8ebe0c7ded0a4804a617a8c8201566d108
Tiago Marques <sr.tiagomarques@gmail.com>
2020-03-16 10:31:29 -0300

    yarn packages updates

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -21,6 +21,7 @@ const Routes = createAppContainer(
     }, {
         defaultNavigationOptions: {
             headerTintColor: '#FFF',
+            headerBackTitleVisible: null,
         headerStyle: {
             backgroundColor: '#7D40E7',
 
